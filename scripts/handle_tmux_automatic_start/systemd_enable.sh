d92726c937f303f46f8802adba6cdf2213af319b
pete 'Peteches' McCabe <pete@peteches.co.uk>
2016-05-19 08:09:21 +0100

    Fixed strange output issues with some commands
    - Some commands such as `ip` or `ssh-keygen -t edcsa` there would be no output
    - strace showed the command writing to stdout.
    - starting the tmux command via bash resolved the issue.

--- scripts/handle_tmux_automatic_start/systemd_enable.sh
+++ scripts/handle_tmux_automatic_start/systemd_enable.sh
@@ -19,7 +19,7 @@ template() {
 	[Service]
 	Type=forking
 	Environment=DISPLAY=:0
-	ExecStart=/usr/bin/tmux ${systemd_tmux_server_start_cmd}
+	ExecStart=/bin/bash -c /usr/bin/tmux ${systemd_tmux_server_start_cmd}
 
 	ExecStop=${HOME}/.tmux/plugins/tmux-resurrect/scripts/save.sh
 	ExecStop=/usr/bin/tmux kill-server

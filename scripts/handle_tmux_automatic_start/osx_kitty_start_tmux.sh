cf5715d5fa7790e0c3b64631e7547d8576e2fc65
Dillon Lees <ddlees@us.ibm.com>
2020-07-07 10:16:53 -0400

    chore: use tabs instead of spaces for formatting

--- scripts/handle_tmux_automatic_start/osx_kitty_start_tmux.sh
+++ scripts/handle_tmux_automatic_start/osx_kitty_start_tmux.sh
@@ -4,44 +4,44 @@
 TRUE_FULL_SCREEN="$1"
 
 start_terminal_and_run_tmux() {
-  osascript <<- EOF
+	osascript <<-EOF
 	tell application "kitty"
 		activate
-    delay 5
-    tell application "System Events" to tell process "kitty"
-      set frontmost to true
-      keystroke "tmux"
-      key code 36
-    end tell
+		delay 5
+		tell application "System Events" to tell process "kitty"
+			set frontmost to true
+			keystroke "tmux"
+			key code 36
+		end tell
 	end tell
 	EOF
 }
 
 resize_window_to_full_screen() {
-  osascript <<- EOF
-tell application "kitty"
-	activate
-	tell application "System Events"
-		if (every window of process "kitty") is {} then
-			keystroke "n" using command down
-		end if
+	osascript <<-EOF
+	tell application "kitty"
+		activate
+		tell application "System Events"
+			if (every window of process "kitty") is {} then
+				keystroke "n" using command down
+			end if
 
-		tell application "Finder"
-			set desktopSize to bounds of window of desktop
+			tell application "Finder"
+				set desktopSize to bounds of window of desktop
+			end tell
+
+			set position of front window of process "kitty" to {0, 0}
+			set size of front window of process "kitty" to {item 3 of desktopSize, item 4 of desktopSize}
 		end tell
-
-		set position of front window of process "kitty" to {0, 0}
-		set size of front window of process "kitty" to {item 3 of desktopSize, item 4 of desktopSize}
 	end tell
-end tell
-EOF
+	EOF
 }
 
 resize_to_true_full_screen() {
-  osascript <<- EOF
+	osascript <<-EOF
 	tell application "kitty"
 		activate
-    delay 1
+		delay 1
 		tell application "System Events" to tell process "kitty"
 			keystroke "f" using {control down, command down}
 		end tell
@@ -50,11 +50,11 @@ resize_to_true_full_screen() {
 }
 
 main() {
-  start_terminal_and_run_tmux
-  if [ "$TRUE_FULL_SCREEN" == "fullscreen" ]; then
-    resize_to_true_full_screen
-  else
-    resize_window_to_full_screen
-  fi
+	start_terminal_and_run_tmux
+	if [ "$TRUE_FULL_SCREEN" == "fullscreen" ]; then
+		resize_to_true_full_screen
+	else
+		resize_window_to_full_screen
+	fi
 }
 main

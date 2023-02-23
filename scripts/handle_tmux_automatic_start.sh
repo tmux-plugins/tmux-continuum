0a90b1b554e4529ff11b882dbe1e38bae2a2d30e
Calle Pettersson <calle@cape.nu>
2023-02-23 20:27:46 +0100

    Avoid overriding systemd unit/settings when not configured to do so

--- scripts/handle_tmux_automatic_start.sh
+++ scripts/handle_tmux_automatic_start.sh
@@ -18,17 +18,25 @@ is_systemd() {
 	[ $(ps -o comm= -p1) == 'systemd' ]
 }
 
+# Only manage the systemd unit either if it is not already installed, or if it
+# was installed by us (as signified by the magic comment)
+should_manage_systemd_unit() {
+	[ ! -f ${systemd_unit_file_path} ] \
+		&& \
+	systemctl cat --user ${systemd_service_name} | grep -q 'managed-by:tmux-continuum'
+}
+
 main() {
 	if is_tmux_automatic_start_enabled; then
 		if is_osx; then
 			"$CURRENT_DIR/handle_tmux_automatic_start/osx_enable.sh"
-		elif is_systemd; then
+		elif is_systemd && should_manage_systemd_unit; then
 			"$CURRENT_DIR/handle_tmux_automatic_start/systemd_enable.sh"
 		fi
 	else
 		if is_osx; then
 			"$CURRENT_DIR/handle_tmux_automatic_start/osx_disable.sh"
-		elif is_systemd; then
+		elif is_systemd && should_manage_systemd_unit; then
 			"$CURRENT_DIR/handle_tmux_automatic_start/systemd_disable.sh"
 		fi
 	fi
